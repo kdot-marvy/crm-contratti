5e87d16a6219b1469dc0fe9ea681e226e0d2c1af
Bernard Amankwah <bernardamankwah@hotmail.com>
2021-11-01 01:32:12 +0100

    interfaccia contratti

--- src/app/admin/components/elenco-contracts/new-contract/new-contract.component.ts
+++ src/app/admin/components/elenco-contracts/new-contract/new-contract.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-new-contract',
   templateUrl: './new-contract.component.html',
@@ -11,5 +12,6 @@ export class NewContractComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 
 }
