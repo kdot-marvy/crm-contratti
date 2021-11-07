91c6c248b9a38047f28d855e674ea6656077f651
Bernard Amankwah <bernardamankwah@hotmail.com>
2021-11-07 01:46:04 +0100

    contract table

--- src/app/admin/components/icons/icons.component.ts
+++ src/app/admin/components/icons/icons.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'icons-cmp',
     moduleId: module.id,
-    templateUrl: 'icons.component.html'
+    templateUrl: 'icons.component.html',
+    styleUrls: ['./icons.component.css']
 })
 
-export class IconsComponent{}
+export class IconsComponent{
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router
+    ){}
+
+    goToNew(){
+        this.router.navigate(['signup'])
+    }
+}
